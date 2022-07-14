68e355dfec4a465e1d865436fee946112cebf6c7
guanjian <guanjian@bytedance.com>
2022-07-14 10:41:33 +0800

    test: add serialize test case

--- util/serialize_test.go
+++ util/serialize_test.go
@@ -5,11 +5,17 @@ import (
 	"testing"
 )
 
-func TestJsonSerializer_Marshal(t *testing.T) {
-	type User struct {
-		Name string
-		Age int
-	}
+type User struct {
+	Name string
+	Age  int
+}
 
-	fmt.Println(Marshal(&User{Name: "zhangsan",Age: 20}))
+func TestJsonSerializer_Marshal(t *testing.T) {
+	fmt.Println(Marshal(&User{Name: "zhangsan", Age: 20}))
+}
+
+func TestJsonSerializer_Unmarshal(t *testing.T) {
+	u := &User{}
+	Unmarshal("{\"Name\":\"zhangsan\",\"Age\":20}", u)
+	fmt.Println(u)
 }
